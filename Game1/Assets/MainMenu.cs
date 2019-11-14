bf4eef50f33a4be2028ea613d4a61a39a2f4e936
Niina <t7hani01@students.oamk.fi>
2019-11-14 12:36:20 +0200

    mainmenu works now fully
    its missing only help section

--- Game1/Assets/MainMenu.cs
+++ Game1/Assets/MainMenu.cs
@@ -11,7 +11,6 @@ public class MainMenu : MonoBehaviour{
 
     public void QuitGame(){
         Debug.Log("Quit game");
-        UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
 }
