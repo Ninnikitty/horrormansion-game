266a1402481a4a3d6d919d7642c104bdf6b1e1ac
alex <t7raal00@students.oamk.fi>
2019-11-25 22:34:43 +0200

    inventory base template
    inventory template and script to camera; pressing i opens the inventory, character stops moving. esc cancels it and lets the character move again

---
+++ Game1/Assets/Inventory.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] public GameObject invHolder;
+    [SerializeField] public bool usingInv;
+    [SerializeField] public GameObject movement;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I)) //Press I to activate inventory
+        {
+            //if (GameManager.gameManager.inGameFunction) return;
+            usingInv = true;
+            ShowInv();
+            stopMovement();
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && usingInv) //Press esc to cancel inventory
+        {
+            usingInv = false;
+            HideInv();
+            allowMovement();
+        }
+    }
+
+    void ShowInv()
+    {
+        invHolder.SetActive(true);
+        //SceneManager.LoadScene(Inventory);
+    }
+
+    void HideInv() //hide inventory
+    {
+        invHolder.SetActive(false); //canvas state false
+    }
+
+    void allowMovement()
+    {
+        movement = GameObject.Find("mrs_template_mk2"); //find the game object (character)
+        movement.GetComponent<CharacterMovement>().enabled = true; //allow the movement script to run
+    }
+
+    void stopMovement()
+    {
+        movement = GameObject.Find("mrs_template_mk2"); //find the game object (character)
+        movement.GetComponent<CharacterMovement>().enabled = false; //disable the movement script
+    }
+}
