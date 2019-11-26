8f746af8a6aee1599184206cfe570b12f37bb905
Spop22 <olli.as@outlook.com>
2019-11-26 17:49:52 +0200

    Fixed life
    I hope

--- Game1/Assets/CharacterMovement.cs
+++ Game1/Assets/CharacterMovement.cs
@@ -2,95 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour{
+public class CharacterMovement : MonoBehaviour
+{
 
     float speed = 0;
     float rotSpeed = 80;
-    float gravity = 1000;
+    float gravity = 600;
     float rot = 0;
-    bool crouched = false;
 
-    float crouchSpeed = 1;
-    float walkSpeed = 3;
-    float sprintSpeed = 6;
-    float walkSpeedBack = -2;
-    float crouchSpeedBack = -1;
+    float walkSpeed = 2;
+    float sprintSpeed = 4;
+    float walkSpeedBack = -1;
+    float timeTillIdleDefault = 10;
+    float timeTillIdle;
 
-<<<<<<< HEAD
     bool in3rdOerson = true;
-    int timeTillIdle, timeTillIdleDefault = 20;
 
-=======
->>>>>>> parent of 787ecb0... Commit of player camera
     KeyCode forward = KeyCode.W;
     KeyCode backwards = KeyCode.S;
     KeyCode sprint = KeyCode.LeftShift;
     KeyCode crouch = KeyCode.LeftControl;
+    KeyCode switchCamera = KeyCode.C;
 
     Vector3 moveDirection = Vector3.zero;
 
     CharacterController ctrl;
     Animator animator;
+    cameraController cs;
     // Start is called before the first frame update
-    void Start(){
+    void Start()
+    {
         ctrl = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
-<<<<<<< HEAD
         cs = FindObjectOfType<cameraController>();
         timeTillIdle = timeTillIdleDefault;
-=======
-        crouched = animator.GetBool("crouched");
->>>>>>> parent of 787ecb0... Commit of player camera
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
 
-        //Check if crouch is pressed and toggle crouch
-        if (Input.GetKey(crouch))
-        {
-            animator.SetBool("crouched", !animator.GetBool("crouched"));
-            crouched = animator.GetBool("crouched");
-            if (crouched)
-            {
-                animator.SetInteger("playerAnimState", 3);
-            }
-        }
+        timeTillIdle -= Time.deltaTime;
 
         if (Input.GetKey(forward))
         {
-            if (Input.GetKey(sprint) && !crouched){
+            if (Input.GetKey(sprint))
+            {
                 animator.SetInteger("playerAnimState", 2);
                 speed = sprintSpeed;
+                cs.runningCamera(true);
             }
-            else if (!crouched) {
+            else
+            {
                 speed = walkSpeed;
                 animator.SetInteger("playerAnimState", 1);
             }
-<<<<<<< HEAD
             if (Input.GetKeyUp(sprint))
             {
                 cs.runningCamera(false);
-=======
-            else if (crouched)
-            {
-                speed = crouchSpeed;
-                animator.SetInteger("playerAnimState", 6);
->>>>>>> parent of 787ecb0... Commit of player camera
             }
         }
         else if (Input.GetKey(backwards))
         {
-            if (!crouched)
-            {
-                animator.SetInteger("playerAnimState", 1);
-                speed = walkSpeedBack;
-            }
-            else if (crouched)
-            {
-                animator.SetInteger("playerAnimState", 6);
-                speed = crouchSpeedBack;
-            }
+            animator.SetInteger("playerAnimState", 1);
+            speed = walkSpeedBack;
         }
         //After checking for position and setting speed and animation, move the character
         moveDirection = new Vector3(0, 0, 1);
@@ -104,16 +79,15 @@ public class CharacterMovement : MonoBehaviour{
             animator.SetInteger("playerAnimState", 0);
             moveDirection = Vector3.zero;
         }
-        if(Input.GetKeyUp(backwards))
+        if (Input.GetKeyUp(backwards))
         {
             speed = 0;
             animator.SetInteger("playerAnimState", 0);
             moveDirection = Vector3.zero;
         }
-<<<<<<< HEAD
         if (timeTillIdle < 0)
         {
-            animator.SetInteger("playerAnimState", - 1);
+            animator.SetInteger("playerAnimState", -1);
             timeTillIdle = timeTillIdleDefault;
         }
         if (Input.GetKeyUp(switchCamera))
@@ -121,9 +95,7 @@ public class CharacterMovement : MonoBehaviour{
             cs.SwitchCamera(in3rdOerson);
             in3rdOerson = !in3rdOerson;
         }
-        
-=======
->>>>>>> parent of 787ecb0... Commit of player camera
+
         rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
         transform.eulerAngles = new Vector3(0, rot, 0);
         moveDirection.y -= gravity * Time.deltaTime;
---
+++ Game1/Assets/cameraController.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    Camera cam;
+    Vector3 thirdPLocation = new Vector3(0.25f, 1.95f, -0.50f);
+    Vector3 firstPLocation = new Vector3(0, 1.75f, 0.25f);
+    bool currently3rd = true;
+
+    private float firstPSens = 5f;
+    private float thirdPSens = 5f;
+    private float maxYAngle = 80f;
+    public Transform Player;
+    public GameObject PlayerObj;
+    private float mouseX, mouseY;
+
+    private float yOffset = 15f;
+
+    private Vector2 currentRotation;
+
+
+
+    void Start()
+    {
+        cam = gameObject.GetComponent<Camera>();
+    }
+
+
+    private void LateUpdate()
+    {
+        CameraControl();
+    }
+
+    void CameraControl()
+    {
+        if (!currently3rd)
+        {
+            mouseX += Input.GetAxis("Mouse X") * thirdPSens;
+            mouseY -= Input.GetAxis("Mouse Y") * thirdPSens;
+            mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+            Vector3 targetPostition = new Vector3(Player.position.x,
+                                           this.transform.position.y,
+                                           Player.position.z);
+            this.transform.LookAt(targetPostition);
+            this.transform.rotation = Quaternion.Euler(this.transform.rotation.x + mouseY, this.transform.rotation.y, this.transform.rotation.z);
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+        else if(currently3rd){
+            mouseX += Input.GetAxis("Mouse X") * thirdPSens;
+            mouseY -= Input.GetAxis("Mouse Y") * thirdPSens;
+            mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+            Vector3 targetPostition = new Vector3(Player.position.x,
+                                           this.transform.position.y,
+                                           Player.position.z);
+            this.transform.LookAt(targetPostition);
+            this.transform.rotation = Quaternion.Euler(this.transform.rotation.x+10+mouseY, this.transform.rotation.y+yOffset, this.transform.rotation.z);
+            Player.rotation = Quaternion.Euler(0, mouseX, 0);
+        }
+    }
+
+    public void SwitchCamera(bool toFirst)
+    {
+        if (toFirst)
+        {
+            cam.transform.localPosition = firstPLocation;
+            cam.transform.localRotation = new Quaternion(0, 0, 0, 0);
+        }
+        else if (!toFirst)
+        {
+            cam.transform.localPosition = thirdPLocation;
+            cam.transform.localRotation = new Quaternion(0, 0, 0, 0);
+        }
+        currently3rd = !currently3rd;
+    }
+    public void runningCamera(bool running)
+    {
+        Vector3 pos = new Vector3();
+        if (currently3rd)
+        {
+            pos = new Vector3(thirdPLocation.x, thirdPLocation.y - 0.2f, thirdPLocation.z + 0.1f);
+        }
+        else if (!currently3rd)
+        {
+            pos = firstPLocation;
+        }
+
+        if (running)
+        {
+            if (!currently3rd)
+            {
+                PlayerObj.layer = 9;
+            }
+            cam.transform.localPosition = pos;
+        }
+        else if (!running)
+        {
+            PlayerObj.layer = 0;
+            if (currently3rd)
+            {
+                cam.transform.localPosition = thirdPLocation;
+            }
+            else if (!currently3rd)
+            {
+                cam.transform.localPosition = firstPLocation;
+            }
+        }
+    }
+}
